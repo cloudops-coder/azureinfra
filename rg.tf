a356324a7d5f6069503671241b450dc0d9a12e73
bsnlmahesh <mahesh.23joy@gmail.com>
2025-07-14 20:58:21 +0530

    classic rg added

--- rg.tf
+++ rg.tf
@@ -9,26 +9,17 @@ provider "azurerm" {
   subscription_id = "93358ad1-cda0-434e-8a1b-0f6ccc5bfcc0"
   
 }
-resource "azurerm_resource_group" "rg1" {
-  name     = "rg1"
+resource "azurerm_resource_group" "classic1" {
+  name     = "classic1"
   location = "central india"
 
 }
 # nitesh added 1 more line
-resource "azurerm_resource_group" "rg4" {
-  name     = "rg4"
+resource "azurerm_resource_group" "classic2" {
+  name     = "classic2"
 
   location = "central india"
 
 }
 
-resource "azurerm_resource_group" "rg2" {
-  name     = "rg2"
-  location = "central india"
-
-}
-resource "azurerm_resource_group" "rg3" {
-  name     = "rg3"
-  location = "east us"
-}
 
