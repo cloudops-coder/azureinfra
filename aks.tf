1438b70a196ccae316e0f5471653353e8e557b05
Mahesh Pandey <98970605+cloudops-coder@users.noreply.github.com>
2026-04-18 17:24:41 +0530

    aks code added
    Added aks code to terraform

--- aks.tf
+++ aks.tf
@@ -1,1 +1,20 @@
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "my-aks-cluster"
+  location            = "central india"
+  resource_group_name = "classic1"
+  dns_prefix          = "myaks"
 
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
